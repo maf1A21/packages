f9f8b3001af8a4c4147449ab9377a3091ed69984
Jurabek Aminov <61906886+maf1A21@users.noreply.github.com>
2022-05-31 14:40:59 +0300

    Update anime.py

--- anime.py
+++ anime.py
@@ -5,9 +5,11 @@ import pandas as pd
 data_frame = pd.read_csv('anime.csv')
 print(data_frame.dtypes)
 
+#-------------------------------------------------------------------------------------------------------------------
 data_frame = data_frame.rename(columns={"Airdate": "air_date"})
 data_frame.columns = data_frame.columns.str.lower()
 
+#-------------------------------------------------------------------------------------------------------------------
 title = 'title'
 production = 'production'
 episodes = 'episodes'
@@ -31,6 +33,7 @@ print(f'\nFor production:\n{data_frame[production].value_counts()}')
 print(f'\nFor source:\n{data_frame[source].value_counts()}')
 print(f'\nFor theme:\n{data_frame[theme].value_counts()}')
 
+#-------------------------------------------------------------------------------------------------------------------
 data_frame[data_frame[air_date]==""] = np.NaN
 data_frame[air_date] = data_frame[air_date].fillna('Not defined')
 
@@ -40,24 +43,33 @@ data_frame[episodes] = data_frame[episodes].fillna('Not defined')
 data_frame[data_frame[theme]=="-"] = np.NaN
 data_frame[theme] = data_frame[theme].fillna('Smth else')
 
-# 8(a)
-data_frame[production].value_counts().sort_values().plot(kind='bar')
+#-------------------------------------------------------------------------------------------------------------------
 
-#8(b)
-fig = plt.figure('Episodes')
-ep = data_frame['episodes'].value_counts()
+fig = plt.figure('Production', figsize=(16, 9))
+prod = data_frame[production].sort_values().value_counts()[:20]
+prod.plot.bar()
+#-------------------------------------------------------------------------------------------------------------------
+fig = plt.figure('Episodes', figsize=(16, 9))
+ep = data_frame[episodes].value_counts()[:20]
 ep.plot.bar()
+#-------------------------------------------------------------------------------------------------------------------
+fig2 = plt.figure('Source', figsize=(16, 9))
+sr = data_frame[source].value_counts()[:20]
+sr.plot.bar()
+#-------------------------------------------------------------------------------------------------------------------
+fig3 = plt.figure('Rating', figsize=(16, 9))
+rmean = data_frame[rating].groupby(data_frame[production]).mean()
+rmean = rmean.sort_values(ascending = False)[:20]
+rmean.plot.bar()
+#-------------------------------------------------------------------------------------------------------------------
+fig4 = plt.figure('Rating Intervals', figsize=(16, 9), dpi=120)
+rt = data_frame['rating'].groupby(by=(data_frame['rating'].apply(np.floor)), dropna=True).count()
+rt.plot.bar()
 
-#ep = df['episodes'].value_counts()
-ep.plot.barh()
-#8c
-fig = plt.figure('Source')
-sr = df['source'].value_counts()[:30]
-#sr = df['source'].value_counts()
-sr.plot.barh()
-#8d
-fig = plt.figure('Theme')
-masked = set(df['theme'])
+#-------------------------------------------------------------------------------------------------------------------
+
+fig = plt.figure('Best themes ', figsize=(16, 9), dpi=120)
+masked = set(data_frame['theme'])
 et = set()
 for comb in masked:
     if type(comb) == str:
@@ -65,46 +77,7 @@ for comb in masked:
             et.add(s)
 themes = dict()
 for theme in et:
-    themes[theme] = df['theme'].str.contains(theme, na=False, regex=False).sum()
-t = list()
-for key in themes:
-    t.append((key, themes[key]))
-t = sorted(t, key = lambda x: x[1])
-val = list()
-label = list()
-for tup in t:
-    label.append(tup[0])
-    val.append(tup[1])
-plt.barh(range(len(t)), val, tick_label=label)
-#8e
-fig = plt.figure('Year')
-yr = df['airdate'].groupby(df['airdate'].dt.year).count()
-yr.plot()
-
-print(9) #9
-fig = plt.figure('Rating')
-rmean = df['rating'].groupby(df['production']).mean()
-rmean = rmean.sort_values(ascending = False)[:30]
-#rmean = rmean.sort_values(ascending = False)
-rmean.plot.barh()
-
-print(10) #10
-fig = plt.figure('Rating Intervals')
-rt = df['rating'].groupby(by=(df['rating'].apply(np.floor)), dropna=True).count()
-rt.plot.barh()
-
-print(11) #11
-
-fig = plt.figure('Cool Themes Where Rating > 8')
-masked = set(df['theme'])
-et = set()
-for comb in masked:
-    if type(comb) == str:
-        for s in comb.split(','):
-            et.add(s)
-themes = dict()
-for theme in et:
-    themes[theme] = df['theme'].str.contains(theme, na=False, regex=False).where(df['rating'] > 8).sum()
+    themes[theme] = data_frame['theme'].str.contains(theme, na=False, regex=False).where(data_frame['rating'] > 7.5).sum()
 t = list()
 for key in themes:
     t.append((key, themes[key]))
@@ -116,8 +89,8 @@ for tup in t:
     val.append(tup[1])
 plt.barh(range(len(t)), val, tick_label=label)
 
-fig = plt.figure('Cool Genres Where Rating > 8')
-masked = set(df['genre'])
+fig = plt.figure('Best genres', figsize=(16, 9), dpi=120)
+masked = set(data_frame['genre'])
 et = set()
 for comb in masked:
     if type(comb) == str:
@@ -125,7 +98,7 @@ for comb in masked:
             et.add(s)
 themes = dict()
 for theme in et:
-    themes[theme] = df['genre'].str.contains(theme, na=False, regex=False).where(df['rating'] > 8).sum()
+    themes[theme] = data_frame['genre'].str.contains(theme, na=False, regex=False).where(data_frame['rating'] > 7.5).sum()
 t = list()
 for key in themes:
     t.append((key, themes[key]))
@@ -137,8 +110,8 @@ for tup in t:
     val.append(tup[1])
 plt.barh(range(len(t)), val, tick_label=label)
 
-fig = plt.figure('Themes Rating Average')
-masked = set(df['theme'])
+fig = plt.figure('Themes Rating Average', figsize=(16, 9), dpi=120)
+masked = set(data_frame['theme'])
 et = set()
 for comb in masked:
     if type(comb) == str:
@@ -146,7 +119,7 @@ for comb in masked:
             et.add(s)
 themes = dict()
 for theme in et:
-    themes[theme] = df['rating'].where(df['theme'].str.contains(theme, na=False, regex=False)).mean()
+    themes[theme] = data_frame['rating'].where(data_frame['theme'].str.contains(theme, na=False, regex=False)).mean()
 t = list()
 for key in themes:
     t.append((key, themes[key]))
@@ -158,8 +131,8 @@ for tup in t:
     val.append(tup[1])
 plt.barh(range(len(t)), val, tick_label=label)
 
-fig = plt.figure('Genres Rating Average')
-masked = set(df['genre'])
+fig = plt.figure('Genres Rating Average', figsize=(16, 9), dpi=120)
+masked = set(data_frame['genre'])
 et = set()
 for comb in masked:
     if type(comb) == str:
@@ -167,7 +140,7 @@ for comb in masked:
             et.add(s)
 themes = dict()
 for theme in et:
-    themes[theme] = df['rating'].where(df['genre'].str.contains(theme, na=False, regex=False)).mean()
+    themes[theme] = data_frame['rating'].where(data_frame['genre'].str.contains(theme, na=False, regex=False)).mean()
 t = list()
 for key in themes:
     t.append((key, themes[key]))
@@ -179,55 +152,15 @@ for tup in t:
     val.append(tup[1])
 plt.barh(range(len(t)), val, tick_label=label)
 
-print(12) #12
-fig = plt.figure('Voters Rating')
-x = list(df['voters'])
-y = list(df['rating'])
-plt.plot(x, y)
+#-------------------------------------------------------------------------------------------------------------------
 
-print(13) #13
-fig = plt.figure('Themes Voters')
-masked = set(df['theme'])
-et = set()
-for comb in masked:
-    if type(comb) == str:
-        for s in comb.split(','):
-            et.add(s)
-themes = dict()
-for theme in et:
-    themes[theme] = df['voters'].where(df['theme'].str.contains(theme, na=False, regex=False)).mean()
-t = list()
-for key in themes:
-    t.append((key, themes[key]))
-t = sorted(t, key = lambda x: x[1])
-val = list()
-label = list()
-for tup in t:
-    label.append(tup[0])
-    val.append(tup[1])
-plt.barh(range(len(t)), val, tick_label=label)
+fig8 = plt.figure('corelation between voters and ratings', figsize=(16, 9))
+x = list(data_frame['voters'])
+y = list(data_frame['rating'])
+plt.scatter(x, y)
 
-fig = plt.figure('Genres Voters')
-masked = set(df['genre'])
-et = set()
-for comb in masked:
-    if type(comb) == str:
-        for s in comb.split(','):
-            et.add(s)
-themes = dict()
-for theme in et:
-    themes[theme] = df['voters'].where(df['genre'].str.contains(theme, na=False, regex=False)).mean()
-t = list()
-for key in themes:
-    t.append((key, themes[key]))
-t = sorted(t, key = lambda x: x[1])
-val = list()
-label = list()
-for tup in t:
-    label.append(tup[0])
-    val.append(tup[1])
-plt.barh(range(len(t)), val, tick_label=label)
-
+#-------------------------------------------------------------------------------------------------------------------
 
 plt.show()
+
 
